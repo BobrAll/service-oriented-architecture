fa49879d2f21e5069c39ca0a29475588cb3b132c
bobr <bobrus91@gmail.com>
2024-09-12 21:43:03 +0300

    Created template of navigator-microservice

---
+++ navigator-microservice/src/main/java/bobr/navigatorMicroservice/NavigatorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package bobr.navigatorMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigatorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigatorMicroserviceApplication.class, args);
+	}
+
+}
