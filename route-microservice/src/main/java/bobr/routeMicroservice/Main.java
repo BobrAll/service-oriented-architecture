6c262639b7e3f021ae14d2b668bb54528224cb58
bobr <bobrus91@gmail.com>
2024-10-04 00:55:46 +0300

    Added consul auto-registration

--- route-microservice/src/main/java/bobr/routeMicroservice/Main.java
+++ route-microservice/src/main/java/bobr/routeMicroservice/Main.java
@@ -2,16 +2,9 @@ package bobr.routeMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
+public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
